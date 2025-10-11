a766039b6346fc56a2bb0c16b70340fd0aea1863
hengage <henrychizobaudeh@gmail.com>
2025-10-11 18:59:05 +0100

    Feat(shapes): Implement Circle, Rectangle, Square, and Shape classes with area calculation and color management

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using Shapes;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Square square1 = new("Red", 4);
+        Rectangle rectangle1 = new("Blue", 4, 5);
+        Circle circle1 = new("Green", 3);
+        
+        List<Shape> shapes = new List<Shape>
+        {
+            square1,
+            rectangle1,
+            circle1
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}\n");
+        }
     }
 }
